63761072912fdda10814d54296f02bb66c9ec1e1
Pederio19 <Pederiocristan_bscs@plmun.edu.ph>
2025-02-19 22:08:36 -0800

    Submitting OBJPROG Week 05 - Laboratory # 12

---
+++ MethodDemo.java
@@ -0,0 +1,20 @@
+public class MethodDemo {
+
+    public static void printMessage(String message) {
+
+    }
+    public static void main(String[] args) {
+
+        greet();
+        System.out.println("This is my message!"); 
+
+    }
+
+    public static void greet() {
+        System.out.println("Hello from the greet method!"); 
+
+    }
+
+
+
+}
